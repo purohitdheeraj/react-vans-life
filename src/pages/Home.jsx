e341dc77a094a7957d006aaf176db47ed7528dd9
Dheeraj Purohit <purohitdheeraj89@gmail.com>
2023-04-07 16:53:19 +0530

    Feat: Home Page ✅

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-	return <div>Home</div>;
+	return (
+		<section className="home-page">
+			<div className="home-info">
+				<h2>
+					You got the travel plans, we got the travel vans.
+				</h2>
+				<p>
+					Add adventure to your life by joining the #vanlife
+					movement. Rent the perfect van to make your
+					perfect road trip.
+				</p>
+				<Link to="vans" className="link-btn home-cta">
+					Find Your Van
+				</Link>
+			</div>
+		</section>
+	);
 };
 
 export default Home;
